57512b021944eb68a1dab8593734d667e82ec942
ali-attar78 <**YourEmail**>
2021-07-11 21:56:09 +0430

    update1 splash screen

---
+++ app/src/main/java/com/example/truthordare/MyConstant.java
@@ -0,0 +1,13 @@
+package com.example.truthordare;
+
+import android.content.res.Resources;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyConstant  {
+
+    Resources.getSystem().getDisplayMetrics().widthPixels;
+
+
+
+}
