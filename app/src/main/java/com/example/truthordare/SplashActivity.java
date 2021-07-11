dc97403899e807f5e888050b138e89973f66bb09
ali-attar78 <**YourEmail**>
2021-07-11 21:02:07 +0430

    add splash screen

--- app/src/main/java/com/example/truthordare/SplashActivity.java
+++ app/src/main/java/com/example/truthordare/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.truthordare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import java.time.Duration;
 
@@ -13,7 +15,13 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        final int Duration = 3000;
+        final int Duration = 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        },Duration);
 
 
 
