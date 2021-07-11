fbec0a7fe59536d0d8b155b97dcfdb35f3695209
ali-attar78 <**YourEmail**>
2021-07-11 20:53:19 +0430

    initial project

---
+++ app/src/main/java/com/example/truthordare/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.truthordare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.time.Duration;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final int Duration = 3000;
+
+
+
+    }
+}
