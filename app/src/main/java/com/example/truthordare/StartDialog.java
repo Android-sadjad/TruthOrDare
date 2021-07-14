cf7f52d094f499626c5a20a53af4daee56665698
Mohammad <mohammadyt1379@gmail.com>
2021-07-14 19:58:47 +0430

    test program

--- app/src/main/java/com/example/truthordare/StartDialog.java
+++ app/src/main/java/com/example/truthordare/StartDialog.java
@@ -3,25 +3,77 @@ package com.example.truthordare;
 import android.app.Dialog;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class StartDialog extends Dialog {
     ConstraintLayout clStartDialog;
+    SeekBar sbPlayerNumber;
+    ConstraintLayout[] clPlayerNames;
+
 
     public StartDialog(@NonNull Context context) {
         super(context);
         setContentView(R.layout.dialog_start);
         this.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
         findViews();
         setSize();
+        configuraion();
+
+    }
+
+    private void configuraion() {
+
+        sbPlayerNumber.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                for(int i=2;i<progress;i++){
+
+                    Toast.makeText(getContext(), String.valueOf(progress), Toast.LENGTH_SHORT).show();
+                   // clPlayerNames[i].setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
     }
 
     private void findViews() {
 
+
         clStartDialog = findViewById(R.id.cl_start_dialog);
+        sbPlayerNumber = findViewById(R.id.sb_player_number);
+
+        clPlayerNames=new ConstraintLayout[9];
+
+        clPlayerNames[0] = findViewById(R.id.cl_player_name_1);
+        clPlayerNames[1] = findViewById(R.id.cl_player_name_2);
+        clPlayerNames[2] = findViewById(R.id.cl_player_name_3);
+        clPlayerNames[3] = findViewById(R.id.cl_player_name_4);
+        clPlayerNames[4] = findViewById(R.id.cl_player_name_5);
+        clPlayerNames[5] = findViewById(R.id.cl_player_name_6);
+        clPlayerNames[6] = findViewById(R.id.cl_player_name_7);
+        clPlayerNames[7] = findViewById(R.id.cl_player_name_8);
+        clPlayerNames[8] = findViewById(R.id.cl_player_name_9);
+
     }
 
     public void setSize() {
