2fc9323f9d38b38a2b5e21d81f7a413c66c56c52
Mohammad Yazdi <mohammadyt1379@gmail.com>
2021-07-29 13:28:29 +0430

    initialize tapsell advertising

--- app/src/main/java/com/example/truthordare/classes/MyConstant.java
+++ app/src/main/java/com/example/truthordare/classes/MyConstant.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Point;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.example.truthordare.R;
 
 public class MyConstant {
 
@@ -18,6 +23,21 @@ public class MyConstant {
         return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
 
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+        boolean connect=activeNetworkInfo != null && activeNetworkInfo.isConnected();
+
+        if(!connect)
+            Toast.makeText(context, R.string.need_internet, Toast.LENGTH_SHORT).show();
+
+        return connect;
+    }
+
     public static final int BOTTLE_NUMBER=10;
+
+    public static final String TAPSELL_KEY="kqeejibsfcireicosnsakmkrfpcspehismbolbqqmdfjetsrbdqjtphnhmendqotnreedh";
 
 }
