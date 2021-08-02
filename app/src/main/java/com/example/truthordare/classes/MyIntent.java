ba3ec7a9656e969cce8fab0085a51e8f78418c78
Mohammad <mohammadyt1379@gmail.com>
2021-08-02 16:38:57 +0430

    add MyIntent class to project

---
+++ app/src/main/java/com/example/truthordare/classes/MyIntent.java
@@ -0,0 +1,48 @@
+package com.example.truthordare.classes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
+
+
+
+import static android.os.Build.VERSION_CODES.R;
+
+public class MyIntent {
+
+
+    public static void shareAppIntent(Context context) {
+        try {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+
+            String shareMessage = "لینک دانلود برنامه " + "زیارت اربعین(متن و صوت)" + "\n\n";
+            shareMessage += "http://cafebazaar.ir/app/?id=" + "com.ttp.salawatcount" + "\n\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            context.startActivity(Intent.createChooser(shareIntent, "اشتراک گذاری برنامه"));
+        } catch (Exception e) {
+          //  Toast.makeText(context, R.string.error, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void otherAppIntent(Context context) {
+        try {
+            Intent otherAppIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://cafebazaar.ir/developer/mohammad9731"));
+            context.startActivity(otherAppIntent);
+        } catch (Exception e) {
+
+            //Toast.makeText(context, R.string.need_internet, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void commentIntent(Context context) {
+        try {
+            Intent commentIntent = new Intent(Intent.ACTION_EDIT, Uri.parse("bazaar://details?id="+ "com.ttp.ziaratarbaeen"));
+            context.startActivity(commentIntent);
+        } catch (Exception e) {
+           // Toast.makeText(context, R.string.need_install_cafebazaar, Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
